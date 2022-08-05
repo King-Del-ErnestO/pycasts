109aec9bad962a091435ca8cb73b1605804c087d
King-Del-ErnestO <delernestoa@gmail.com>
2022-08-05 17:50:57 +0100

    initial commit

--- podcasts/models.py
+++ podcasts/models.py
@@ -9,7 +9,7 @@ class Episode(models.Model):
     link = models.URLField(max_length=255)
     image = models.URLField(max_length=255)
     podcast_name = models.CharField(max_length=100)
-    guid = models.CharField(max_length=50)
+    guid = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f"{self.podcast_name}: {self.title}"
